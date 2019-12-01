c71c97bf5057993995ffd28222f92452866810f2
Garrick Henne <garrickhenne@outlook.com>
2019-12-01 15:43:47 -0800

    Current Build: Works as intended, no bugs so far. TODO: Add UI and settings?

--- background.js
+++ background.js
@@ -1,25 +1,52 @@
 console.log("background running");
 
-//chrome.tabs.onUpdated.addListener(buttonClicked);
+var isWaitingToUnmute = false;
+var currentMutedTab;
+var currentPlayingTab;
 
-
-
-//TODO: use tabs.query to get list of tabs?
-chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, change, firstTab) => {
   /*
   "Destructuring assignment", same as:
     const audible = change.audible
     const mutedInfo = change.mutedInfo
   */
-  chrome.tabs.query( {} , function (tabs) {
-    tabs.forEach(function(tab) {
-      if ((tab.audible || tab.mutedInfo && !tab.mutedInfo.muted) && tab.id !== tabId && change.hasOwnProperty("audible")) {
-        chrome.tabs.update(tab.id, {muted: true});
-
-      }
-    })
-  })
+ if (change.hasOwnProperty("audible")) {
+    muteInactiveTabs(firstTab,change);
+  }
 });
+
+//JS forgets currentMutedTab and currentPlayingTab, find a way to get info
+chrome.tabs.onUpdated.addListener((tabId, change, playingTab) => {
+  if (isWaitingToUnmute) {
+    chrome.tabs.get(currentPlayingTab, function(currentPlayingTabTab) {
+      if (playingTab.id === currentPlayingTabTab.id && !currentPlayingTabTab.audible) {
+        chrome.tabs.update(currentMutedTab, {muted: false});
+        console.log(`Successfully UNMUTED: ${currentMutedTab}`);
+        isWaitingToUnmute = false;
+        return;
+      }
+    });
+  }
+})
+
+
+function muteInactiveTabs(firstTab,change) {
+  chrome.tabs.query({}, function (tabs) {
+    tabs.forEach(function (tab) {
+      if (tab.audible && tab.id !== firstTab.id && !isWaitingToUnmute) {
+        chrome.tabs.update(tab.id, { muted: true });
+        console.log(`TAB MUTED: ${tab.url}`);
+        console.log(`TAB PLAYING: ${firstTab.url}`);
+        currentMutedTab   = tab.id;
+        currentPlayingTab = firstTab.id;
+        isWaitingToUnmute = true;
+        console.log(`Current Muted Tab: ${currentMutedTab}`);
+        console.log(`isWaitingToUnMute is currently: ${isWaitingToUnmute}`);
+        return;
+      }
+    });
+  });
+}
 
 
 // const { audible, mutedInfo } = change;
