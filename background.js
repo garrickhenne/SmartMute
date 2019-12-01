52264bf257725762798f1b96588a3368a12e81de
Garrick Henne <garrickhenne@outlook.com>
2019-11-30 22:09:58 -0800

    Added Comment

--- background.js
+++ background.js
@@ -5,11 +5,16 @@ chrome.browserAction.onClicked.addListener(buttonClicked);
 
 
 //TODO: use tabs.query to get list of tabs?
-chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+chrome.tabs.onUpdated.addEventListener((tabId, change, tab) => {
+  /*
+  "Destructuring assignment", same as:
+    const audible = change.audible
+    const mutedInfo = change.mutedInfo
+  */
   const { audible, mutedInfo } = change;
   if (audible || mutedInfo && !mutedInfo.muted) {
     chrome.tabs.update(tabId, {muted: true});
-  } 
+  }
 });
 
 function buttonClicked(tab) {
