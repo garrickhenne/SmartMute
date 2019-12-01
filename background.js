185327fbcd347daa2e354f3a5dec99ab300fd4ae
Garrick Henne <garrickhenne@outlook.com>
2019-12-01 00:06:16 -0800

    Current Functionality: Mutes inactive tab TODO: Unmute when finished

--- background.js
+++ background.js
@@ -1,22 +1,27 @@
 console.log("background running");
 
-chrome.browserAction.onClicked.addListener(buttonClicked);
+//chrome.tabs.onUpdated.addListener(buttonClicked);
 
 
 
 //TODO: use tabs.query to get list of tabs?
-chrome.tabs.onUpdated.addEventListener((tabId, change, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
   /*
   "Destructuring assignment", same as:
     const audible = change.audible
     const mutedInfo = change.mutedInfo
   */
-  const { audible, mutedInfo } = change;
-  if (audible || mutedInfo && !mutedInfo.muted) {
-    chrome.tabs.update(tabId, {muted: true});
-  }
+  chrome.tabs.query( {} , function (tabs) {
+    tabs.forEach(function(tab) {
+      if ((tab.audible || tab.mutedInfo && !tab.mutedInfo.muted) && tab.id !== tabId && change.hasOwnProperty("audible")) {
+        chrome.tabs.update(tab.id, {muted: true});
+
+      }
+    })
+  })
 });
 
-function buttonClicked(tab) {
-  console.log(tab);
-}
+
+// const { audible, mutedInfo } = change;
+//           if (audible || mutedInfo && !mutedInfo.muted) {
+//           chrome.tabs.update(tabs[i].id, {muted: true});
